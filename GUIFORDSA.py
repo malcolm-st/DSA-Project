3a4f2e4e47feea1f99f6fc428eb32ad488b4f0f7
Farhan <minecraftfarhan@gmail.com>
2023-07-10 22:46:41 +0800

    Organized vendor analysis into separate .py

--- GUIFORDSA.py
+++ GUIFORDSA.py
@@ -31,6 +31,7 @@ import webbrowser
 
 from tkinter import *
 from SystemChecker import *
+from VendorAnalysis import *
 
 # Global flag variable
 is_program_running = True
@@ -780,43 +781,6 @@ def search_cve(search_text, sort_by=None, results_text=None):
 
     # Display the filtered results in the Text widget
     results_text.insert(tk.END, filtered_data.to_string(index=False))
-
-def vendor_frequency_analysis(num_items):
-    vendor_counts = {}
-
-    # Opens CSV file to carry out frequency analysis on most frequent vendors
-    with open('CVECSV.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            vendor = row['Vendor']
-            # Do not show vendors with 'n/a' or empty fields
-            if vendor != 'n/a' and vendor != '':
-                if vendor in vendor_counts:
-                    vendor_counts[vendor] += 1
-                else:
-                    vendor_counts[vendor] = 1
-
-    sorted_vendors = list(vendor_counts.items())
-    sorted_vendors = merge_sort(sorted_vendors)[::1]
-    
-    top_vendors = sorted_vendors[:num_items]  # Select number of vendors defined by user to display
-
-    top_vendors = top_vendors[::-1]  # Reverse the list to show highest frequency at the top in the chart
-
-    vendors = [vendor for vendor, count in top_vendors]
-    frequencies = [count for vendor, count in top_vendors]
-
-    plt.barh(vendors, frequencies)  # Create a horizontal bar chart
-
-    # Display the frequency value beside each bar
-    for i, freq in enumerate(frequencies):
-        plt.text(freq, i, str(freq), va='center')
-
-    plt.xlabel('Frequency')  # Label for the x-axis
-    plt.ylabel('Vendor')  # Label for the y-axis
-    plt.title("Top "+str(num_items)+" Vendors by Frequency")  # Title of the plot
-    plt.tight_layout()  # Ensure labels fit within the plot
-    # plt.show()  # Display the plot
 
 
 # Analysis Page
