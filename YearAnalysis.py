5aa4f307d0ebc0907ec9401fc1491c31a086dc0c
Teng-Jun <122024473+Teng-Jun@users.noreply.github.com>
2023-07-19 21:21:47 +0800

    Added merge sort

--- YearAnalysis.py
+++ YearAnalysis.py
@@ -2,6 +2,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+
+#############################################################################################
+#######################                                            ##########################
+#######################             Year Analysis for App          ##########################
+#######################                                            ##########################
+#############################################################################################
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i][1] > right[j][1]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
+
+# The function will create the chart
 def create_year_chart(year):
     # Check if the chart attribute exists in the year frame
     chart_attribute_exists = hasattr(year, "chart")
@@ -22,15 +60,15 @@ def create_year_chart(year):
     # Count the number of CVEs for each year
     year_counts = df['Year'].value_counts()
 
-    # Find the top five years with the most CVEs
-    top_five_years = year_counts.nlargest(5).sort_values()
+    # Sort the years in ascending order based on the number of CVEs
+    sorted_years = dict(sorted(year_counts.items(), key=lambda x: x[1]))
 
-    # Display the CVE details for the top five years
-    top_five_years_cves = df[df['Year'].isin(top_five_years.index)]
+    # Extract the top five years with the most CVEs
+    top_five_years = dict(list(sorted_years.items())[-5:])
 
     # Create a bar chart of the top five years with the most CVEs
     fig, ax = plt.subplots()
-    ax.bar(top_five_years.index, top_five_years.values)
+    ax.bar(top_five_years.keys(), top_five_years.values())
     ax.set_xlabel('Year')
     ax.set_ylabel('Number of CVEs')
     ax.set_title('Top Five Years with the Most CVEs')
